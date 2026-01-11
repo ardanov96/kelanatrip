46d204eca362ebaed32def299b65634d91e733e7
ardanov96 <ardanov96@gmail.com>
2026-01-11 19:09:45 +0800

    add nav and video

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="relative w-full h-[120vh] sm:h-[100vh]">
+        {/* overlay */}
+      <div className="absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70"></div>
+        {/* Video */}
+      <video 
+            src="/images/hero1.mp4" 
+            autoPlay 
+            muted 
+            loop 
+            preload="metadata" 
+            className='w-full h-full objective-cover'
+        />
+        {/* Text content */}
+        <div className="absolute z-[100] w-full h-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <div className="flex items-center justify-center flex-col w-full h-full">
+                <div>
+                    <h1 className='text-[25px] mb-4 md:mb-0 
+                    text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] 
+                    text-white font-bold uppercase'>Let's Enjoy The Nature
+                    </h1>
+                    <p className="md:text-base text-center text-lg text-white font-formal [word-spacing:5px]">
+                        Get the best prices on 2,000,000+ properties, worldwide
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,12 +1,33 @@
+"use client";
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TbAirBalloon } from 'react-icons/tb'
 import { navLinks } from '@/constant/constant';
 import { HiBars3BottomRight } from 'react-icons/hi2';
 
-const Nav = () => {
+type Props = {
+    openNav:()=>void
+};
+
+const Nav = ({openNav}: Props) => {
+
+    const [navBg,setNavBg]=useState(false);
+
+    useEffect(()=>{
+        const handler =()=>{
+            if(window.scrollY>=90) setNavBg(true);
+            if(window.scrollY<90) setNavBg(false);
+        };
+        window.addEventListener("scroll", handler);
+        return () => window.removeEventListener("scroll", handler);
+    },[]);
+
+
   return (
-    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]'>
+    <div 
+        className={`bg-blue-950 ${
+        navBg ? "bg-blue-950 shadow-md" : "fixed"
+    } transition-all duration-200 h-[12vh] z-[1000] fixed w-full`}>
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
             {/* LOGO */}
             <div className="flex items-center space-x-2">
@@ -39,7 +60,7 @@ const Nav = () => {
                     Book Now 
                 </button>
                 {/* Burger Menu */}
-                <HiBars3BottomRight className="" />
+                <HiBars3BottomRight onClick={openNav} className="w-8 h-8 cursor-pointer text-white lg:hidden" />
             </div>
         </div>
     </div>
